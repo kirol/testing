de8e9776bd351771f3df221a71dc021a900baf00
unknown <minh.lq7@gmail.com>
2017-02-02 04:49:58 +0700

    'abc'

--- impact analysis/Programmatic-CDA/src/org/pfsw/tools/cda/examples/Sample1.java
+++ impact analysis/Programmatic-CDA/src/org/pfsw/tools/cda/examples/Sample1.java
@@ -36,6 +36,12 @@ import org.pf.tools.cda.core.processing.IProgressMonitor;
 import org.pf.tools.cda.core.processing.WaitingIElementsProcessingResultHandler;
 import org.pf.util.SysUtil;
 import org.apache.commons.lang3.ArrayUtils;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import java.io.File; import java.io.FileWriter; 
+import java.io.IOException; 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 /**
  * An example for working with CDA without GUI.
@@ -45,81 +51,90 @@ import org.apache.commons.lang3.ArrayUtils;
  */
 public class Sample1
 {
-  // =========================================================================
-  // CONSTANTS
-  // =========================================================================
-  private static final boolean IS_MONITORING = false;
-  private static final String CLASS_TO_ANALYZE = "com.jgoodies.common.collect.ArrayListModel";
+	// =========================================================================
+	// CONSTANTS
+	// =========================================================================
+	private static final boolean IS_MONITORING = false;
+	private static final String CLASS_TO_ANALYZE = "com.jgoodies.common.collect.ArrayListModel";
 
-  // =========================================================================
-  // INSTANCE VARIABLES
-  // =========================================================================
+	// =========================================================================
+	// INSTANCE VARIABLES
+	// =========================================================================
 
-  // =========================================================================
-  // CLASS METHODS
-  // =========================================================================
-  public static void main(String[] args) throws InterruptedException, IOException
-  {
-	 ArrayList<String> arr = new ArrayList();
-	 
-	 JarFile jarFile = new JarFile("example-libs/jgoodies-common-1.8.0.jar");
-     Enumeration enumeration = jarFile.entries();
-     while (enumeration.hasMoreElements()) {
-	    if(processJarFiles(enumeration.nextElement()).endsWith(".class"))
-	    {
-	    	String cname = processJarFiles(enumeration.nextElement()).replace('/', '.');
-	    	String className = cname.replace(".class", "");
-	    	arr.add(className);
-	    }
-     }
-     
-     System.out.println(arr);
-     
-     
-     
-	
-    CommandLineArguments commandArgs;
-    Sample1 inst;
+	// =========================================================================
+	// CLASS METHODS
+	// =========================================================================
+	public static void main(String[] args) throws InterruptedException, IOException
+	{
 
-    inst = new Sample1();
-    /*commandArgs = new CommandLineArguments(args);
+
+
+
+		ArrayList<String> arr = new ArrayList();
+
+		JarFile jarFile = new JarFile("example-libs/LibraryJTP.jar");
+		Enumeration enumeration = jarFile.entries();
+		while (enumeration.hasMoreElements()) {
+			if(processJarFiles(enumeration.nextElement()).endsWith(".class"))
+			{
+				String cname = processJarFiles(enumeration.nextElement()).replace('/', '.');
+				String className = cname.replace(".class", "");
+				arr.add(className);
+			}
+		}
+
+
+
+
+
+
+		CommandLineArguments commandArgs;
+		Sample1 inst;
+		List<String> implementArray = new ArrayList<String>();
+		List<String> extendtArray = new ArrayList<String>();
+		List<String> useArray = new ArrayList<String>();
+
+
+		inst = new Sample1();
+		/*commandArgs = new CommandLineArguments(args);
     inst.run(commandArgs);*/
-    inst.run(arr);
-    System.err.flush();
-    System.out.flush();
-    SysUtil.current().exit(0, 100);
-  } // main()
+		inst.run(arr);
 
-  // =========================================================================
-  // CONSTRUCTORS
-  // =========================================================================
-  public Sample1()
-  {
-    super();
-  } 
+		System.err.flush();
+		System.out.flush();
+		SysUtil.current().exit(0, 100);
+	} // main()
 
-  // =========================================================================
-  // PROTECTED INSTANCE METHODS
-  // =========================================================================
-  /*protected void run(CommandLineArguments args)
+	// =========================================================================
+	// CONSTRUCTORS
+	// =========================================================================
+	public Sample1()
+	{
+		super();
+	} 
+
+	// =========================================================================
+	// PROTECTED INSTANCE METHODS
+	// =========================================================================
+	/*protected void run(CommandLineArguments args)
   {
     Workset workset;
     String sample;
-    
+
     // Create a workset with a defined classpath
     workset = this.createWorkset();
     // Load all elements on the claspath and pre-analyze them (might take a while!)
     this.initializeWorkset(workset);
 
     sample = args.getArgumentValue("-sample");
-    
+
     if ("1".equals(sample))
     {
       this.showClassesMatching(workset, "F.*r$");
     }
     else if ("2".equals(sample))
     {
-      
+
       this.showDependenciesOf(workset, CLASS_TO_ANALYZE);
     }
     else if ("3".equals(sample))
@@ -131,177 +146,249 @@ public class Sample1
       this.showDependantsOf(workset, CLASS_TO_ANALYZE);
     }
   }*/
-  
-  
-  
-  protected void run(ArrayList<String> arr)
-  {
-    Workset workset;
-    String sample;
-    
-    // Create a workset with a defined classpath
-    workset = this.createWorkset();
-    // Load all elements on the claspath and pre-analyze them (might take a while!)
-    this.initializeWorkset(workset);
 
-    for(String i : arr){
-    	this.showDependenciesOf(workset, i);
-    }
-    /*this.showDependenciesOf(workset, CLASS_TO_ANALYZE);*/
-    
-    
-  }
-  
-  
 
-  protected void showClassesMatching(Workset workset, String namePattern) 
-  {
-    Pattern pattern = Pattern.compile(namePattern);
-    ClassNameFinder processor;
-    List<ClassInformation> result = new ArrayList();
-    ClassInformation[] classes;
 
-    processor = new ClassNameFinder(pattern);
-    workset.processClassInformationObjects(result, processor);
-    System.out.println("Scanned " + processor.getResultData() + " classes.");
-    System.out.println("Found " + result.size() + " classes for pattern: " + namePattern);
-    classes = ClassInformation.collectionToArray(result);    
-    this.showResult(classes);    
-  }
-  
-  //
-  protected void showDependenciesOf(Workset workset, String className) 
-  {
-    ClassInformation classInfo;
-    ClassInformation[] allClasses;
-    ClassInformation[] directlyUseClasses;
-    ClassInformation[] directlyImplemntedInterfaces;
-    ClassInformation[] directlyExtendedInterfaces;
-    ClassInformation[] notUniqClasses;
+	protected void run(ArrayList<String> arr)
+	{
+		Workset workset;
+		String sample;
 
-    // Lookup the class of interest
-    classInfo = workset.getClassInfo(className);
 
-   
-    // Get interfaces which are implemented by current class/interface
-    directlyImplemntedInterfaces = classInfo.getDirectlyImplementedInterfaces();
-    /*System.out.println("\n" + classInfo.getName() + " dependes on " + classes.length + " classes:");
-    this.showResult(classes); */   
-    System.out.println("\n" + classInfo.getName() + " implements " + directlyImplemntedInterfaces.length + " interfaces:");
-    this.showResult(directlyImplemntedInterfaces);   
-    List<String> a1 = this.showResult(directlyImplemntedInterfaces);    
-    
-    // Get classes which are extended by current class/interface
-    directlyExtendedInterfaces = classInfo.getDirectlyExtendedInterfaces();
-    System.out.println("\n" + classInfo.getName() + " extends " + directlyExtendedInterfaces.length + " classes:");
-    this.showResult(directlyExtendedInterfaces);  
-    List<String> a2 = this.showResult(directlyExtendedInterfaces);    
-    
-    
-   
-    
-    
-    // Get classes which are used by current class/interface
-    allClasses = classInfo.getReferredClassesArray();
-    System.out.println("\n" + classInfo.getName() + " uses " + allClasses.length + " classes:");
-    List<String> a3 = this.showResult(allClasses); 
-    /*List<String> combined = new ArrayList<String>();
-    combined.addAll(a1);
-    combined.addAll(a2);
-    combined.addAll(a3);
-    System.out.println("\nCombined array is: " + combined);*/
-    ArrayList<String> uniquevalues = new ArrayList<String>(new List(a3).removeAll(a1));  
-    
-  }
-  
-  protected void showAllDependenciesOf(Workset workset, String className) 
-  {
-    ClassInformation classInfo;
-    ClassInformation[] classes;
-    
-    // Lookup the class of interest
-    classInfo = workset.getClassInfo(className);
+		JSONObject obj = new JSONObject();
+		obj.put("class","go.GraphLinksModel");
+		obj.put("nodeKeyProperty", "id");
 
-    IAnalyzableElement elementToAnalyze = classInfo;
-    DependencyAnalyzer analyzer = new DependencyAnalyzer(elementToAnalyze);
-    analyzer.analyze();
-    DependencyInfo result = analyzer.getResult();
+		JSONArray nodeArray = new JSONArray();
+		JSONArray linkArray = new JSONArray();
 
-    classes = result.getAllReferredClasses();    
-    System.out.println(classInfo.getName() + " dependes on " + classes.length + " classes:");
-    this.showResult(classes);    
-  }
-  
-  protected void showDependantsOf(Workset workset, String className) 
-  {
-    ClassInformation classInfo;
-    ClassInformation[] dependants;
-    
-    // Lookup the class of interest
-    classInfo = workset.getClassInfo(className);
-    
-    WaitingIElementsProcessingResultHandler searchHandler = new WaitingIElementsProcessingResultHandler();
-    dependants = searchHandler.findDependantsOfClass(classInfo, "01", null, true);
-    System.out.println(classInfo.getName() + " has " + dependants.length + " classes depending on it:");
-    this.showResult(dependants);    
-  }
-  
-  protected List<String> showResult(ClassInformation[] resultData)
-  {
-	List<String> a1 = new ArrayList();
-    for (ClassInformation classInformation : resultData)
-    {
-      a1.add(classInformation.getName());
-      System.out.print(classInformation.getName());
-    }
-    return a1;
-  }
+		// Create a workset with a defined classpath
+		workset = this.createWorkset();
+		// Load all elements on the claspath and pre-analyze them (might take a while!)
+		this.initializeWorkset(workset);
+		System.out.println(arr);
+		for(String i : arr){
+			JSONObject obj1 = new JSONObject();
+			obj1.put("category","UndesiredEvent");
+			obj1.put("id",i);
+			obj1.put("text",i);
+			obj1.put("color", "lightyellow");
+			nodeArray.add(obj1);
 
-  protected Workset createWorkset()
-  {
-    Workset workset;
-    workset = new Workset("Sample1");
-    ClasspathPartDefinition partDefinition;
-    
-    partDefinition = new ClasspathPartDefinition("example-libs/jgoodies-common-1.8.0.jar");
-    workset.addClasspathPartDefinition(partDefinition);
-    workset.addIgnoreFilter(new StringFilter("java.*"));
-    workset.addIgnoreFilter(new StringFilter("javax.*"));
-    return workset;
-  }
+			List<String> list1 = new ArrayList<String>();
+			List<String> list2 = new ArrayList<String>();
+			List<String> list3 = new ArrayList<String>();
+			Object[] listOfArrays = this.showDependenciesOf(workset, i);
+			list1 = (List<String>)listOfArrays[0];
+			list2 = (List<String>)listOfArrays[1];
+			list3 = (List<String>)listOfArrays[2];
+			
 
-  protected void initializeWorkset(Workset workset)
-  {
-    WorksetInitializer wsInitializer;
-    IProgressMonitor monitor = null;
+			if(list1.isEmpty() == false) {
+				for(String j : list1){
+					JSONObject obj2 = new JSONObject();
+					obj2.put("from",i);
+					obj2.put("color","#2F4F4F");
+					obj2.put("thick", 1.5);
+					obj2.put("category", "Realization");
+					obj2.put("to", j);
+					linkArray.add(obj2);
+					
+					JSONObject obj1_1 = new JSONObject();
+					obj1_1.put("category","UndesiredEvent");
+					obj1_1.put("id",j);
+					obj1_1.put("text",j);
+					obj1_1.put("color", "lightyellow");
+					if(!nodeArray.contains(obj1_1)){
+						nodeArray.add(obj1_1);
+					}
+					
+				}
+			}
+			
+			if(list2.isEmpty() == false) {
+				for(String j : list2){
+					JSONObject obj2 = new JSONObject();
+					obj2.put("from",i);
+					obj2.put("color","#2F4F4F");
+					obj2.put("thick", 1.5);
+					obj2.put("category", "Generalization");
+					obj2.put("to", j);
+					linkArray.add(obj2);
+					
+					JSONObject obj1_1 = new JSONObject();
+					obj1_1.put("category","UndesiredEvent");
+					obj1_1.put("id",j);
+					obj1_1.put("text",j);
+					obj1_1.put("color", "lightyellow");
+					if(!nodeArray.contains(obj1_1)){
+					nodeArray.add(obj1_1);
+					}
+				}
+			}
+			
+			
+			if(list3.isEmpty() == false) {
+				for(String j : list3){
+					JSONObject obj2 = new JSONObject();
+					obj2.put("from",i);
+					obj2.put("color","#2F4F4F");
+					obj2.put("thick", 1.5);
+					obj2.put("category", "DirectedAssociation");
+					obj2.put("to", j);
+					linkArray.add(obj2);
+					
+					JSONObject obj1_1 = new JSONObject();
+					obj1_1.put("category","UndesiredEvent");
+					obj1_1.put("id",j);
+					obj1_1.put("text",j);
+					obj1_1.put("color", "lightyellow");
+					if(!nodeArray.contains(obj1_1)){
+						nodeArray.add(obj1_1);
+					}
+				}
+			}
 
-    if (IS_MONITORING)
-    {      
-      monitor = new ConsoleMonitor();
-    }
-    
-    wsInitializer = new WorksetInitializer(workset);
-    
-    // Running with no progress monitor (null) is okay as well.
-    wsInitializer.initializeWorksetAndWait(monitor); 
-  }
-  
-  
-  
- 
 
-	     private static String processJarFiles(Object obj) {
-	       JarEntry entry = (JarEntry)obj;
-	       String name = entry.getName();
-	       return name;
-	       
-	       
-	       /*long size = entry.getSize();
+
+            
+			
+
+			/*System.out.println(arr);*/
+		}
+		
+		
+		obj.put("nodeDataArray", nodeArray);
+		obj.put("linkDataArray", linkArray);
+		
+		/*this.showDependenciesOf(workset, CLASS_TO_ANALYZE);*/
+		try (FileWriter file = new FileWriter("f:\\test.json")) {
+
+			file.write(obj.toString());
+			file.flush();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+
+
+	// 
+	protected Object[] showDependenciesOf(Workset workset, String className) 
+	{
+		ClassInformation classInfo;
+		ClassInformation[] allClasses;
+		ClassInformation[] directlyUseClasses;
+		ClassInformation[] directlyImplemntedInterfaces;
+		ClassInformation[] directlyExtendedInterfaces;
+		ClassInformation[] notUniqClasses;
+
+		// Lookup the class of interest
+		classInfo = workset.getClassInfo(className);
+
+
+		// Get interfaces which are implemented by current class/interface
+		directlyImplemntedInterfaces = classInfo.getDirectlyImplementedInterfaces();
+		List<String> a1 = this.showResult(directlyImplemntedInterfaces);    
+		/*System.out.println("\n\n" + classInfo.getName() + " implements interfaces:");
+    System.out.println(a1);*/
+
+		// Get classes which are extended by current class/interface
+		directlyExtendedInterfaces = classInfo.getDirectlyExtendedInterfaces();
+		List<String> a2 = this.showResult(directlyExtendedInterfaces);    
+		/*System.out.println("\n\n" + classInfo.getName() + " extends classes:");
+    System.out.println(a2);*/
+
+
+		// Combine two arrays
+		List<String> a12 = new ArrayList<String>(a1);
+		a12.addAll(a2);
+
+		// Get classes which are used by current class/interface
+		allClasses = classInfo.getReferredClassesArray();
+		List<String> a3 = this.showResult(allClasses); 
+
+		// Get unique values
+		List<String> duplicateList = new ArrayList<String>();
+		List<String> uniqueList = new ArrayList<String>();
+		for(String item : a3) {
+			if(a12.contains(item)){
+				duplicateList.add(item);
+			}else{
+				uniqueList.add(item);
+			}
+		}
+		/*System.out.println("\n\n" + classInfo.getName() + " uses classes:");
+    System.out.println(uniqueList);*/
+
+		return new Object[]{a1,a2,uniqueList};
+
+	}
+
+
+
+	protected List<String> showResult(ClassInformation[] resultData)
+	{
+		List<String> a1 = new ArrayList<String>();
+		for (ClassInformation classInformation : resultData)
+		{
+			a1.add(classInformation.getName());
+			/*System.out.print(classInformation.getName());*/
+		}
+		return a1;
+	}
+
+	protected Workset createWorkset()
+	{
+		Workset workset;
+		workset = new Workset("Sample1");
+		ClasspathPartDefinition partDefinition;
+
+		partDefinition = new ClasspathPartDefinition("example-libs/LibraryJTP.jar");
+		workset.addClasspathPartDefinition(partDefinition);
+		workset.addIgnoreFilter(new StringFilter("java.*"));
+		workset.addIgnoreFilter(new StringFilter("javax.*"));
+		return workset;
+	}
+
+
+
+
+
+	protected void initializeWorkset(Workset workset)
+	{
+		WorksetInitializer wsInitializer;
+		IProgressMonitor monitor = null;
+
+		if (IS_MONITORING)
+		{      
+			monitor = new ConsoleMonitor();
+		}
+
+		wsInitializer = new WorksetInitializer(workset);
+
+		// Running with no progress monitor (null) is okay as well.
+		wsInitializer.initializeWorksetAndWait(monitor); 
+	}
+
+
+
+
+
+	private static String processJarFiles(Object obj) {
+		JarEntry entry = (JarEntry)obj;
+		String name = entry.getName();
+		return name;
+
+
+		/*long size = entry.getSize();
 	       long compressedSize = entry.getCompressedSize();*/
-	       
-	     }
-	   
-  
-  
+
+	}
+
+
+
 
 }
